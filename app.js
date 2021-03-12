3e027035e4574e2a7215a277906934ced956baf8
Cosmin-DB <ConstantinCosmin.Dobrescu@alu.uclm.es>
2021-03-12 20:44:45 +0100

    deploy

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
+mongoose.set('useCreateIndex',true);
+mongoose.set('useUnifiedTopology',true);
+require('dotenv').config();
+
+var bodyParser = require('body-parser');
+var postsRouter=require('cors');
+var cors = require('cors');
 
 var app = express();
 
@@ -20,12 +27,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() =>  console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
+
+app.use(cors());
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({limit:'50mb',extended:true}));
 
 // error handler
 app.use(function(err, req, res, next) {
