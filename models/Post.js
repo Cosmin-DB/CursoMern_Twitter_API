652c631f070f919270e1e35dd77b91e77d731f46
Cosmin-DB <ConstantinCosmin.Dobrescu@alu.uclm.es>
2021-03-26 20:14:58 +0100

    fin hoy

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
+
 var PostSchema = new Schema({
   user: String,
   email: String,
--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var router = express.Router();
-var Post = require('../models/Post.js');//Import model.
-var db = mongoose.connection; // Connect to database
 
+var router = express.Router();
+
+//Models
+var Post = require('../models/Post.js');
+
+var db = mongoose.connection;
 
 /* GET posts listing ordered by publicationdate. */
 router.get('/', function (req, res) {
-  Post.find().sort('-publicationdate').exec(function(err, posts) {
-    if (err) res.status(500).send(err);
-    else res.status(200).json(posts);
-  });
+    Post.find().sort('-publicationdate').exec(function(err, posts) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(posts);
+    });
 });
 
 /* GET all posts from an user by user Email */
@@ -35,18 +38,14 @@ router.put('/:id', function (req, res) {
     if (err) res.status(500).send(err);
     else res.sendStatus(200);
   });
-  
+});
+
 /* DELETE an existing post */
 router.delete('/:id', function (req, res) {
   Post.findByIdAndDelete(req.params.id, function (err, postinfo) {
     if (err) res.status(500).send(err);
     else res.sendStatus(200);
   });
-
-});
-
-
-
 });
 
 module.exports = router;
